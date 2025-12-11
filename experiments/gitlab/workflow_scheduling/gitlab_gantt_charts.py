250cabb10b980f3e78e41fe7a2c04ae9d72aa2b4
okafke <65917827+okafke@users.noreply.github.com>
2025-12-11 21:55:13 +0100

    feat(gitlab): pipeline runs from local gitlab instance

--- experiments/gitlab/workflow_scheduling/gitlab_gantt_charts.py
+++ experiments/gitlab/workflow_scheduling/gitlab_gantt_charts.py
@@ -22,12 +22,17 @@ def main():
         print('Creating Gantt chart for run', run_id)
         durations = []
         jobs_url = f"{gitlab_url}/api/v4/projects/{gitlab_project_id}/pipelines/{run_id}/jobs"
-        jobs = requests.get(jobs_url, headers=gitlab_headers).json()
+        response = requests.get(jobs_url, headers=gitlab_headers)
+        if not response.ok:
+            raise Exception(response.reason)
+        jobs = response.json()
+        (experiments_folder / f"run{run_id}.json").write_text(json.dumps(jobs, indent='\t'))
         for job in jobs:
             print('Getting job information for job', job['id'])
             job_id = job['id']
-            detail = requests.get(f"{gitlab_url}/api/v4/projects/{gitlab_project_id}/jobs/{job_id}",
-                                  headers=gitlab_headers).json()
+            detail_response = requests.get(f"{gitlab_url}/api/v4/projects/{gitlab_project_id}/jobs/{job_id}", headers=gitlab_headers)
+            (experiments_folder / f"run{run_id}-{job_id}.json").write_text(json.dumps(jobs, indent='\t'))
+            detail = detail_response.json()
             started = datetime.fromisoformat(detail.get('started_at').replace("Z", "+00:00"))
             finished = datetime.fromisoformat(detail.get('finished_at').replace("Z", "+00:00"))
             durations.append(JobDuration(detail.get('name'), started, finished))
