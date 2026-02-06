97aa3bd6009c3a9993ee5c4444f49deb8001765c
okfk <65917827+okafke@users.noreply.github.com>
2026-02-06 13:26:10 +0100

    feat(simulation): added scenario where LPT is not optimal

--- experiments/simulation/workflow_scheduling/simulation_gantt_charts.py
+++ experiments/simulation/workflow_scheduling/simulation_gantt_charts.py
@@ -1,7 +1,8 @@
 import json
-import os
 from datetime import datetime
 from pathlib import Path
+
+from fontTools.feaLib.lexer import NonIncludingLexer
 
 from experiments.commons import (get_latest_experiment_number, get_experiment_folder, JobDuration,
                                  plot_job_gantt)
@@ -9,6 +10,7 @@ from experiments.commons import (get_latest_experiment_number, get_experiment_fo
 def main():
     output_root = Path(__file__).parent
     output_root.mkdir(exist_ok=True)
+    total_durations = {}
     for directory in [ x for x in output_root.iterdir() if x.is_dir() ]:
         all_jobs = []
         for file in directory.iterdir():
@@ -29,8 +31,22 @@ def main():
                 finished = datetime.fromisoformat(job.get('finished_at').replace("Z", "+00:00"))
                 durations.append(JobDuration(job.get('name'), started, finished))
                 all_jobs.append(JobDuration(job.get('name'), started, finished))
-            plot_job_gantt(durations,f"Step/job durations for run {run_id}", directory / f"gantt_run{run_id}", sort=False)
+            #plot_job_gantt(durations,f"Step/job durations for run {run_id}", directory / f"gantt_run{run_id}", sort=False)
+        if not all_jobs:
+            continue
         plot_job_gantt(all_jobs,f"Step/job durations for all jobs", directory / f"gantt_run_all_jobs", sort=False)
+        jobs_sorted = sorted(all_jobs, reverse=False, key=lambda x: x.start)
+        first = jobs_sorted[0]
+        jobs_sorted = sorted(all_jobs, reverse=True, key=lambda x: x.end)
+        last = jobs_sorted[0]
+        duration = last.end - first.start
+        total_durations[directory.name] = duration
+
+    total_durations = dict(sorted(total_durations.items()))
+    print("---------------------------------")
+    print()
+    for key, value in total_durations.items():
+        print(key, value)
 
 
 if __name__ == "__main__":
